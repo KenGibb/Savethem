add832afb6e9d6dda394085487ee3d5c0303d42b
KenGibb <kenjibbo@gmail.com>
2023-01-06 14:31:13 -0600

    Stuck fixing canvas

--- js/app.js
+++ js/app.js
@@ -1,44 +1,56 @@
-// I need three entities: a savior, a monster, and a person-in-need.
-// the savior needs to be able to move with up and down arrow keys
-// the monster needs to know where the player is to collide and make something happen
-// When monster collides with player, player should lose health/or die if no health left
-// I need obsticles in the game to make it more challenging for player to get to perosn-in-need.
-// If player collides with obsticles, player gets slowed down
-// When player collides with person-in-need, game ends and you win
-////////////////////////////////////////////////////////
+// This is an extension/refactoring of the fruitcakes branch of this repo.
+// more notes and comments are in the code on that branch.
+// this version incorporates smoother movement, and a more open ended collision detection
+// this also has a second enemy to face, which creates a new win condition(kill two ogres)
 
+// requirements and goals 
+// make a simple crawler game using canvas that we manipulate in js
 
-// html elements to refer to later
+// ///////// RULES FOR THE GAME ////////////
+// we need two entities, a savior and an ogre
+// the savior should move with the WASD or ARROW keys(display savior coords)
+// the ogre(for now) will be stationary
+// the savior and the first ogre should be able to collide to make something happen
+// when the savior collides with the first ogre, ogre 1 is removed from the screen, and ogre 2 appears
+// after hitting ogre 2, the game stops, and sends a message to the user that they have won.
+// ////////////// END RULES /////////////////
+
+//////////// INITIAL SETUP /////////////////////
+
+// first we grab our HTML elements for easy reference later
 const game = document.getElementById('canvas')
-const timer = document.getElementById('clock')
-const health = document.getElementById('health')
+const movement = document.getElementById('movement')
+const message = document.getElementById('status')
 const startGame = document.getElementById('button')
 const menu = document.getElementById('button2')
+const health = document.getElementById('canvas2')
 
+// if we want to test if we got the right elements, you can do this:
+canvas2.innerText = 'some stuff'
 
- health.innerText = 'hhhh'
-
+// we need to SET the game's context to be 2d
+// we also want to save that context to a variable for reference later
+// this is how we tell code to work within the context of the canvas
 const ctx = game.getContext('2d')
-ctx.fillStyle = "blue"
-ctx.strokeStyle = "red"
-ctx.lineWidth = 5;
 
+// console.log('game before setting w and H', game)
 
-ctx.fillRect(10, 10, 100, 100);
-ctx.strokeRect(10, 10, 100, 100);
-//console.log('game before setting v and h')
-
+// one thing we need to do, is get the computed size of our canvas
+// then we save that attribute to our canvas so we can refer to it later
+// once we have the exact size of our canvas, we can use those dimensions to simulate movement in interesting ways.
+// these two lines will set the width and height attributes according to the way they look in your browser at the time the code runs
 game.setAttribute('width', getComputedStyle(game)['width'])
 game.setAttribute('height', getComputedStyle(game)['height'])
-game.height = 500
+game.height = 320
+game.width = 480
 
 
-
-///// Person-in-need Class /////
-class Friend {
+//////////// CRAWLER CLASS /////////////////////
+// Since these two objects are basically the same, we can create a class to keep our code dry.
+class Monster  {
     constructor(x, y, width, height, color) {
-        this.x = 10
-        this.y = 10
+        this.x = x
+        this.y = y
         this.width = width
         this.height = height
         this.color = color
@@ -50,24 +62,27 @@ class Friend {
     }
 }
 
-//////// Savior Class /////////
-class Savior {
-    constructor(x, y, width, health, color) {
+class Savior  {
+    constructor(x, y, width, height, color) {
         this.x = x
         this.y = y
         this.width = width
-        this.height = health
+        this.height = height
         this.color = color
         this.alive = true
-        // players speed and direction
-        this.speed = 20
+        //this.maxHealth = maxHealth
+        //this.health = maxHealth
+        // we need additional props on our savior class to make movement smoother
+        this.speed = 15
+        // now we'll add direction, which will be set with our move handler
         this.direction = {
             up: false,
             down: false,
-            right: false,
-            left: false
+            left: false,
+            right: false
         }
-        // the keys that sets the hero in that direction
+        // two other methods, tied to key events
+        // one sets the direction, which sends our savior flying in that direction
         this.setDirection = function (key) {
             console.log('this is the key in setDirection', key)
             if (key.toLowerCase() == 'w') { this.direction.up = true }
@@ -75,7 +90,7 @@ class Savior {
             if (key.toLowerCase() == 's') { this.direction.down = true }
             if (key.toLowerCase() == 'd') { this.direction.right = true }
         }
-        // when you let go it unsets player in the direction
+        // the other unsets a direction, which stops our savior from moving in that dir
         this.unsetDirection = function (key) {
             console.log('this is the key in unsetDirection', key)
             if (key.toLowerCase() == 'w') { this.direction.up = false }
@@ -83,12 +98,13 @@ class Savior {
             if (key.toLowerCase() == 's') { this.direction.down = false }
             if (key.toLowerCase() == 'd') { this.direction.right = false }
         }
-
-        // I will need to set the direction property on savior object
-        this.moveSavior = function () {
+        // this is our new movementHandler, we'll get rid of the old one
+        // this will allow us to use the direction property on our savior object
+        this.movePlayer = function () {
+            // send our guy flyin in whatever direction is true
             if (this.direction.up) {
                 this.y -= this.speed
-                // this is to wall off savior
+                // while we're tracking movement, lets wall off the sides of the canvas
                 if (this.y <= 0) {
                     this.y = 0
                 }
@@ -113,6 +129,7 @@ class Savior {
                 }
             }
         }
+
         this.render = function () {
             ctx.fillStyle = this.color
             ctx.fillRect(this.x, this.y, this.width, this.height)
@@ -120,36 +137,130 @@ class Savior {
     }
 }
 
-/*const getRandomCoordinates = (max) => {
+const getRandomCoordinates = (max) => {
     // we'll use math.random to produce a random number
     return Math.floor(Math.random() * max)
-} */
-
-///////// Monster Class ////////
-// monster will have similar coordinates to player but after a certain time
-class Monster {
-    constructor(x, y, width, height, color) {
-        this.x = x
-        this.y = y
-        this.width = width
-        this.height = height
-        this.color = color
-        this.alive = true
-        this.speed = 15
-        this.render = function () {
-            ctx.fillStyle = this.color
-            ctx.fillRect(this.x, this.y, this,width, this.height)
-            // monster needs to know players coordinates at all times
-            // needs follow function
-        }
-    }
 }
 
-const player = new Savior(50, 50, 16, 16, 'brown')
-const monster = new Monster (200, 50, 32, 48, 'red')
-const friend = new Friend (getRandomCoordinates(game.width), getRandomCoordinates(game.height), 64, 96, 'blue')
+const player = new Savior (10, 10, 16, 16, 'lightsteelblue')
+const ogre = new Monster (200, 50, 32, 48, '#bada55')
+const ogre2 = new Monster (getRandomCoordinates(game.width), getRandomCoordinates(game.height), 64, 96, 'red')
 
-player.render()
-monster.render()
-friend.render()
+////////// Health Bar ///////////
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+const width = canvas.width = 320;
+const height = canvas.height = 480;
 
+canvas.style.marginTop = window.innerHeight / 2 - height / 2 + "px";
+
+let health = 100;
+const healthBarWidth = 200;
+const healthBarHeight = 30;
+const x = width / 2 - healthBarWidth / 2;
+const y = height / 2 - healthBarHeight / 2;
+
+const healthBar = new HealthBar(x, y, healthBarWidth, healthBarHeight, health, "green");
+
+const framed = function() {
+  context.clearRect(0, 0, width, height);
+  healthBar.show(context);
+  requestAnimationFrame(framed);
+}
+
+canvas.onclick = function() {
+  health -= 10;
+  healthBar.updateHealth(health);
+};
+
+framed();
+
+//////////////////////////////////////////
+
+// player.render()
+// ogre.render()
+
+// MOVEMENT HANDLING KEY EVENTS
+
+//////////// COLLISION DETECTION /////////////////////
+// here, we'll detect a hit between entities
+// to accurately do this, we need to account for the entire space that one entity takes up.
+// this means using the player x, y, width and height
+// this also means using the ogre x, y, w, h
+const detectHit = (thing) => {
+    // we'll basically use a big if statement, to be able to tell if any of the sides
+    // of our savior interact with any of the sides of our ogre
+    if (player.x < thing.x + thing.width
+        && player.x + player.width > thing.x
+        && player.y < thing.y + thing.height
+        && player.y + player.height > thing.y) {
+            console.log('HIT!')
+            thing.alive = false
+        }
+}
+
+
+//////////// GAME LOOP /////////////////////
+// we're going to set up a gameLoop function
+// this will be attached to an interval
+// this function will run every interval(amount of ms)
+// this is how we will animate our game
+
+const gameLoop = () => {
+    // no console logs in here if you can avoid it
+    // console.log('the game loop is running')
+    // for testing, it's ok to add them, but final should not have any
+    // putting our hit detection at the top so it takes precedence
+
+    // to resemble movement, we should clear the old canvas every loop
+    // then, instead of drawing a snake because it's maintaining all the old positions of our character
+    // we'll just see our player square moving around
+    ctx.clearRect(0, 0, game.width, game.height)
+
+    if (ogre.alive) {
+        ogre.render()
+        detectHit(ogre)
+    } else if (ogre2.alive) {
+        message.textContent = 'Now Kill Shrek 2!'
+        ogre2.render()
+        detectHit(ogre2)
+    } else {
+        message.textContent = 'You win!'
+        stopGameLoop()
+    }
+
+    player.render()
+    player.movePlayer()
+    movement.textContent = `${player.x}, ${player.y}`
+}
+
+//////////// EVENT LISTENERS /////////////////////
+
+// one key event for a keydown
+// keydown will set a player's direction
+document.addEventListener('keydown', (e) => {
+    // when a key is pressed, set the appropriate direction
+    player.setDirection(e.key)
+})
+// one key event for a keyup
+// keyup, will unset a player's direction
+document.addEventListener('keyup', (e) => {
+    // when a key is released, call unsetDirection
+    // this needs to be handled in a slightly different way
+    if(['w', 'a', 's', 'd'].includes(e.key)) {
+        player.unsetDirection(e.key)
+    }
+})
+
+// we're going to save our game interval to a variable so we can stop it when we want to
+// this interval runs the game loop every 60ms until we tell it to stop
+const gameInterval = setInterval(gameLoop, 60)
+// fn that stops the game loop
+const stopGameLoop = () => { clearInterval(gameInterval) }
+
+// here we'll add an event listener, when the DOMcontent loads, run the game on an interval
+// eventually this event will have more in it.
+document.addEventListener('DOMContentLoaded', function () {
+    // here is our gameloop interval
+    gameInterval
+})
